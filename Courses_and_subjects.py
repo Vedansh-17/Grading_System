522e4925725598bcdb3a8e04b22faed4dad2b031
Vedansh-17 <160649508+Vedansh-17@users.noreply.github.com>
2025-02-21 10:02:42 -0500

    Course FILE

---
+++ Courses_and_subjects.py
@@ -0,0 +1,8 @@
+
+
+courses_and_subjects = {"Computer Engineering Technician": ["Maths","Electrical Fundamentals","Python","Tech Reports", "Practical Circuits"];
+                        "Chemical Laboratory Technician": ["Organic Chemistry", "Statistics", "Microbiology","Inorganic Chemistry","Environmental Science"]; # type: ignore
+                        "International Business Management": ["Financial Analysis", "International Trade Logistics", "Computer Software Applications","Cross-cultural Communications","International Business Law"]; # type: ignore
+                        "Mechanical Engineering": ["Mechanics and Statics","Thermodynamics","Material Science and Engineering","Machine Design","Fluid Mechanics and Hydraulics"]; # type: ignore
+                        "Project Management":["Project Planning","Risk Management","Quality Management","Stakeholder Management","Cost Estimating and Budgeting"]; # type: ignore
+}
